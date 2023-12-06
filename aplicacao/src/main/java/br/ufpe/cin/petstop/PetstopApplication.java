aa01d0a64fbb2c419255c36a09e41a7c9a6e4b5f
Joao Victor Oliveira Santos <jvos@windows.cin.ufpe.br>
2023-12-06 08:49:18 -0300

    feat(aplicacao): Cria aplicação em Springboot

---
+++ aplicacao/src/main/java/br/ufpe/cin/petstop/PetstopApplication.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.petstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstopApplication.class, args);
+	}
+
+}
---
+++ aplicacao/src/test/java/br/ufpe/cin/petstop/PetstopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.petstop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
